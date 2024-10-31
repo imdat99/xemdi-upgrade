c1afec9013cf95faac70e2b8108d982a96a128bd
lethdat <imdat2999@gmail.com>
2024-11-01 01:18:12 +0700

    sort head tag

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -57,17 +57,18 @@ export async function render(
                     <html lang="en" xemdi-theme="${cookies[storageThemeKey]}">
                     <head>
                         <meta charset="utf-8" />
-                        <link rel="icon" href="favicon.ico" />
                         <meta name="theme-color" content="#000000" />
                         <meta name="application-name" content="MOVIE Xemdi" />
                         <meta name="author" content="Xemdi.fun" />
-                        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-                        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-                        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-                        <link rel="manifest" href="/site.webmanifest">
-                        <link rel="preconnect" href="https://apii.online">
-                        <link rel="preconnect" href="https://ophim1.com">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />`)
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <link rel="icon" href="favicon.ico" />
+                        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                        <link rel="preconnect" href="https://apii.online"/>
+                        <link rel="preconnect" href="https://ophim1.com"/>
+                        <link rel="manifest" href="/site.webmanifest"/>
+                        `)
             + styleStr
             + fastRefresh 
             + scripts
